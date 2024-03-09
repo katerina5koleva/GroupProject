ecf1ed09852ca458da6870b2adc0ac34f415c9b0
Katerina Koleva <katikoleva1124@gmail.com>
2024-03-09 16:34:38 +0200

    windows forms app

---
+++ FlowingIdeas/FlowingIdeas/StartUp.cs
@@ -0,0 +1,17 @@
+namespace FlowingIdeas
+{
+    internal static class StartUp
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
