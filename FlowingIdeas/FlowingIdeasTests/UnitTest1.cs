3e789ca16c8aaac9fa667ffece6ea867aaf85650
nadejdasubeva <nadi151006@gmail.com>
2024-03-09 17:34:44 +0200

    hi

---
+++ FlowingIdeas/FlowingIdeasTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FlowingIdeasTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
